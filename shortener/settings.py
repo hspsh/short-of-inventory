196bb0efd311efe8f88e10e3c62437a4ffd6e04f
Norbert Szulc <norbert@not7cd.net>
2021-03-27 17:25:30 +0100

    Minor fixes

--- shortener/settings.py
+++ shortener/settings.py
@@ -3,4 +3,4 @@ base_url = "i.hsp.sh"
 
 main_url = "//hsp.sh"
 wiki_url = "//wiki.hsp.sh/short-of-inventory"
-git_url = "//github.com/hsp/short-of-inventory"
+git_url = "//github.com/hspsh/short-of-inventory"
